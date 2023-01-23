2ea1add0a00d8c1e99b751e507d9844bd9b23307
João Lima <106890966+eujoaolima@users.noreply.github.com>
2023-01-23 15:54:32 -0300

    Update index.tsx

--- src/components/Sections/Menu/index.tsx
+++ src/components/Sections/Menu/index.tsx
@@ -8,7 +8,7 @@ import { useState } from "react"
 const foodItems = [
   {
     id: 1,
-    title: 'Lasagna Bolognesa',
+    title: 'Lasagna Bolognese',
     price: '15.95',
     imgSrc: Lasagna,
     imageURL: Lasagna,
